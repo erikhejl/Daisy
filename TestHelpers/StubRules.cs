0ccf1993505898c6bd3e061ae63cb63b2e2cdede
Mitchell Harris <mitchellkharris@gmail.com>
2013-06-06 11:04:34 -0600

    Adding stub rules in tests

---
+++ TestHelpers/StubRules.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ancestry.Daisy.TestHelpers
+{
+    using Ancestry.Daisy.Rules;
+
+    public class StubRules<T> : RuleController<T>
+    {
+        public bool t()
+        {
+            return true;
+        }
+
+        public bool f()
+        {
+            return true;
+        }
+    }
+
+    public class StubRules : RuleController<int>
+    {
+        public bool odd()
+        {
+            return Scope % 2 == 1;
+        }
+
+        public bool even()
+        {
+            return !odd();
+        }
+    }
+}
